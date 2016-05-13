783a785b98c6dbe9fe5f39f8a3cbb2fd0e5b1ab0
gregd49er <gregdiaz@hotmail.com>
2016-05-13 02:36:39 -0700

    230

---
+++ portfolio.dev/wp-content/plugins/cpt_query.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.6
+ */
+/*
+Plugin Name: Articles Query
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Matt Mullenweg
+Version: 1.6
+Author URI: http://ma.tt/
+*/
+
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+ 
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'article' ) );
+    return $query;
+}
+
+
+?>
--- portfolio.dev/wp-content/themes/blank-responsive-bootstrap-wordpress-theme/index.php
+++ portfolio.dev/wp-content/themes/blank-responsive-bootstrap-wordpress-theme/index.php
@@ -1,25 +1,31 @@
 <?php get_header(); ?>
 
 
+<div class="homearticle"
+	<div class="row">
 
-<div class="row">
-
-	<div class="col-md-3">
+		<div class="col-md-3">
 
 		<img src="http://portfolio.dev/wp-content/uploads/2016/05/articles.png" alt="articles" class="img-responsive">
 
-	</div>
+		</div>
 
-	
-	<div class="homearticle">
-		<main role="main">
-	<!-- section -->
-	<section>
+		<div class="col-md-9">
+			<div class="articleinside">
+			<main role="main">
+			<!-- section -->
+			<section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<?php
+			$args = array( 'post_type' => 'article', 'posts_per_page' => 3 );
+			$the_query = new WP_Query( $args );
+			?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php if ( $the_query->have_posts() ) : ?>
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 			<!-- post thumbnail -->
 			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
@@ -30,53 +36,50 @@
 			<!-- /post thumbnail -->
 
 			<!-- post title -->
-			<h1>
+			<h2>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
+			</h2>
 			<!-- /post title -->
+
+			<!-- post excerpt -->
+			<h3>
+			<?php the_excerpt(); ?></a>
+			</h3>
+			<!-- post excerpt -->
 
 			<!-- post details -->
 			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 			<!-- /post details -->
 
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
 
 			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 
 			<?php comments_template(); ?>
 
-		</article>
-		<!-- /article -->
+			</article>
+			<!-- /article -->
 
-	<?php endwhile; ?>
+			<?php endwhile; ?>
 
-	<?php else: ?>
+			<?php else: ?>
 
-		<!-- article -->
-		<article>
+			<!-- article -->
+			<article>
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+				<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 
-		</article>
-		<!-- /article -->
+			</article>
+			<!-- /article -->
 
-	<?php endif; ?>
+			<?php endif; ?>
 
-	</section>
-	<!-- /section -->
-	</main>
+			</section>
+			<!-- /section -->
+			</main>
 
-
+		</div>
 	</div>
-	</div>
+</div>
 	
 
 	
